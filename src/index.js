08b6477da6fb71fac21bb887a264cfc8788c6614
Jamie Nicholls <jnicholl@massey.ac.nz>
2021-12-17 09:58:48 +1300

    add child tables

--- src/index.js
+++ src/index.js
@@ -2,14 +2,68 @@ import Knex from 'knex';
 import schema from './schema.js';
 import config from './config.js';
 
-// Convert joi schema to simple array of objects 
-const schemaDescription = schema.describe();
-const keys = Object.getOwnPropertyNames(schemaDescription.keys)
-  .map(fieldGroupName => ({
-    tableName: fieldGroupName,
-    tableFields: schemaDescription.keys[fieldGroupName].items[0].keys,
-  }))
+const getDatabaseTableFieldSchema = (tableName, tables, joiFields) => {
+  const tableFields = [];
+  Object.getOwnPropertyNames(joiFields).forEach(joiField => {
+    const field = joiFields[joiField];
 
+    if (field.type === 'array') {
+      if (field.items && field.items[0].keys) {
+        tables.push({
+          tableName: `${tableName}_${joiField}`,
+          tableFields: getDatabaseTableFieldSchema(`${tableName}_${joiField}`, tables, field.items[0].keys),
+        });
+      } else {
+        tables.push({
+          tableName: `${tableName}_${joiField}`,
+          tableFields: getDatabaseTableFieldSchema(`${tableName}_${joiField}`, tables, { value: { type: 'string' } }),
+        });
+      }
+    }
+    tableFields.push({
+      fieldName: joiField,
+      type: field.type,
+      unique: field.type === 'array' ? true : false,
+      primaryKey: (field.rules || []).find(rule => rule.name === 'pk') ? true : false,
+      forginKey: (field.rules || []).find(rule => rule.name === 'fk') ? (field.rules || []).find(rule => rule.name === 'fk').args.path : null,
+      ...field,
+    });
+  });
+  return tableFields;
+}
+
+const getDatabaseTableSchema = (tables) => {
+  const joiSchemaDescription = schema.describe();
+  Object.getOwnPropertyNames(joiSchemaDescription.keys)
+    .forEach(fieldGroupName => {
+      const joiFields = joiSchemaDescription.keys[fieldGroupName].items[0].keys;
+      const tableFields = getDatabaseTableFieldSchema(fieldGroupName, tables, joiFields);
+      tables.push({
+        tableName: fieldGroupName,
+        tableFields: tableFields,
+      });
+    });
+}
+
+
+const createTableField = (tf, table) => {
+  if (tf.type === 'string') {
+    table.string(tf.fieldName);
+    table.unique(tf.fieldName);
+  } else if (tf.type === 'date') {
+    table.datetime(tf.fieldName);
+  } else if (tf.type === 'number') {
+    table.decimal(tf.fieldName);
+  } else if (tf.type === 'boolean') {
+    table.boolean(tf.fieldName);
+  } else if (tf.type === 'array') {
+    table.string(tf.fieldName);
+    table.unique(tf.fieldName);
+  } else {
+    table.string(tf.fieldName);
+    table.unique(tf.fieldName);
+  }
+}
 
 const buildDatabase = async () => {
   const db = Knex({
@@ -17,28 +71,56 @@ const buildDatabase = async () => {
     connection: config.connection,
   });
 
-  for await (const key of keys) {
-    const tableExists = await db.schema.hasTable(key.tableName);
+  const databaseTables = [];
+  getDatabaseTableSchema(databaseTables);
+  // Build database tables
+  for await (const databaseTable of databaseTables) {
+    const tableExists = await db.schema.hasTable(databaseTable.tableName);
     if (!tableExists) {
-      console.log(`Creating table: ${key.tableName}`)
-      await db.schema.createTable(key.tableName, function (table) {
-        Object.getOwnPropertyNames(key.tableFields).forEach(tf => {
-          table.string(tf);
+      console.log(`Creating table: ${databaseTable.tableName}`)
+      await db.schema.createTable(databaseTable.tableName, table => {
+        databaseTable.tableFields.forEach(tf => {
+          createTableField(tf, table);
         });
+        const primaryKeys = databaseTable.tableFields.filter(tf => tf.primaryKey).map(tf => tf.fieldName);
+        if (primaryKeys.length) {
+          table.primary(primaryKeys);
+        }
       });
     } else {
-      for await (const tf of Object.getOwnPropertyNames(key.tableFields)) {
-        const columnExists = await db.schema.hasColumn(key.tableName, tf);
+      for await (const tf of databaseTable.tableFields) {
+        const columnExists = await db.schema.hasColumn(databaseTable.tableName, tf.fieldName);
         if (!columnExists) {
-          console.log(`Adding column (${tf}) to table (${key.tableName})`)
-          await db.schema.alterTable(key.tableName, table => {
-            table.string(tf);
+          console.log(`Adding column (${tf.fieldName}) to table (${databaseTable.tableName})`)
+          await db.schema.alterTable(databaseTable.tableName, table => {
+            createTableField(tf, table);
           })
         }
       };
     }
   };
+  // Add forgin keys
+  for await (const databaseTable of databaseTables) {
+    for await (const tf of databaseTable.tableFields) {
+      await db.schema.alterTable(databaseTable.tableName, table => {
+        if (tf.forginKey) {
+          let tableName = tf.forginKey.split('.[].')[0];
+          let tableReferenece = tf.forginKey.split('.[].')[1];
+          if (tf.forginKey.split('.[].').length === 3) {
+            tableName = `${tf.forginKey.split('.[].')[0]}_${tf.forginKey.split('.[].')[1]}`;
+            tableReferenece = tf.forginKey.split('.[].')[2];
+          }
+
+          console.log(`Adding reference on (${tf.fieldName}) to (${tableReferenece}) in table (${tableName}) for table (${databaseTable.tableName})`)
+          table.foreign(tf.fieldName).references(tableReferenece).inTable(tableName);
+        }
+      })
+    }
+  };
+
   db.destroy();
 }
 
 await buildDatabase();
+
+
